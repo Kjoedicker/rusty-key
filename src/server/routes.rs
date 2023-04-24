d86fd5cbbef3106700c7859a41200e8030dc17c5
kjoedicker <kjoedicker@merkleinc.com>
2023-04-23 22:02:01 -0400

    fix(server/routes): `None` does not indicate a conflict. This change adds a catch all match line for the time being

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -22,7 +22,8 @@ async fn set_key(params: web::Path<(String, String)>) -> impl Responder {
     let mut teller = KEY_VALUE_STORE.lock().unwrap();
 
     match teller.set(key, value, Some(true)) {
-        Some(value) => HttpResponse::Created(),
-        None => HttpResponse::Conflict()
+        // TODO: revisit this when its a little 
+        // more clear when an issue occurs
+        _ => HttpResponse::Created()
     }
 }
