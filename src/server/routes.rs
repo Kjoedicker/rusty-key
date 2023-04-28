5866e9db155ee82b0ecdd739b55cf7c004920302
kjoedicker <kjoedicker@merkleinc.com>
2023-04-27 22:45:17 -0400

    feat(kv_store, server/*): add routing and logic needed for the `delete` functionality

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -1,5 +1,5 @@
 use crate::kv_store::KEY_VALUE_STORE;
-use actix_web::{get, web, Responder, post, HttpResponse};
+use actix_web::{get, web, Responder, post, HttpResponse, delete};
 
 #[get("/{key}")]
 async fn get_key(key: web::Path<String>) -> HttpResponse {
@@ -25,5 +25,23 @@ async fn set_key(params: web::Path<(String, String)>) -> impl Responder {
         // TODO: revisit this when its a little 
         // more clear when an issue occurs
         _ => HttpResponse::Created()
+    }
+}
+
+#[delete("{key}")]
+async fn delete_key(params: web::Path<String>) -> impl Responder {
+    let key = params.into_inner();
+    
+    println!("Received DELETE request Key: {}", key);
+
+    let mut teller = KEY_VALUE_STORE.lock().unwrap();
+
+    match teller.delete(&key, Some(true)) {
+        Some(_) => {
+            HttpResponse::Ok().body("Key deleted")
+        },
+        None => {
+            HttpResponse::NotFound().body("Key not found")
+        }
     }
 }
